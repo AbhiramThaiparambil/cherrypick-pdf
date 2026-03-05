30e9158bad3376f5f8e518c055187c44c218f492
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-03-05 15:40:48 +0530

    chore(server): integrate morgan request logger and add uploads folder to .gitignore

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,11 @@ import "reflect-metadata";
 import "./container";
 import express from "express";
 import pdfRoute from "./presentation/routes/pdf.routes";
+import morgan from "morgan";
 const app = express();
+
 import cors from "cors";
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use("/", pdfRoute);
