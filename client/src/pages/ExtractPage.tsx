319163e384a9c7af5a29e0defa02001a650fae05
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-03-06 21:58:00 +0530

    feat(client): complete PDF extract functionality

--- client/src/pages/ExtractPage.tsx
+++ client/src/pages/ExtractPage.tsx
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Spinner } from "@/components/ui/spinner";
+import { useEffect, useState } from "react";
 import ThumbnailCard from "@/components/extract/ThumbnailCards";
 import SelectedPagesSidebar from "@/components/extract/SelectedPagesSidebar";
+import { useParams } from "react-router";
+import { generateExtractPdf, getPdfthumbnails } from "@/services/pdfservices";
+import Pagination from "@/components/shared/Pagination";
+import Header from "@/components/extract/Header";
+import { toast } from "sonner";
+import { downloadFile } from "@/lib/downloadFile";
+import { ThumbnailSkeleton } from "@/components/extract/Skeleton/ThumbnailSkeleton";
+import { SidebarSkeleton } from "@/components/extract/Skeleton/SelectedPagesSidebarSkeleton";
 
-export type IThumbnails = { thumbnail: string; page: number }[];
+export type IThumbnail = { thumbnail: string; page: number };
 
 export interface IGetPdfThumbnailsDTO {
   thumbnails: { thumbnail: string; page: number }[];
@@ -13,19 +19,32 @@ export interface IGetPdfThumbnailsDTO {
 }
 
 const ExtractPage = () => {
-  const [thumbnails, setThumbnails] = useState<IThumbnails>([]);
-  const [selectedPages, setSelectedPages] = useState<number[]>([]);
+  const [thumbnails, setThumbnails] = useState<IThumbnail[]>([]);
+  const [selectedPages, setSelectedPages] = useState<IThumbnail[]>([]);
+  const [isgenerateNewPdf, setGenerateNewPdf] = useState(false);
+  const [pdfDetails, setPdfDetails] = useState<{
+    totalCount: number;
+    fileName: string;
+  }>({ fileName: "", totalCount: 0 });
+  const [page, setPage] = useState(1);
+
   const [loading, setLoading] = useState(true);
+  const limit = 6;
+  const { id } = useParams();
 
   useEffect(() => {
+    if (!id) {
+      return;
+    }
     const fetchThumbnails = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:4040/pdf/69a96b148f2449e0acdb3a12/thumbnails",
-        );
-
+        const response = await getPdfthumbnails(id, limit, page);
         if (response.status === 200) {
-          setThumbnails(response.data?.thumbnails || []);
+          setPdfDetails({
+            fileName: response.data.fileName,
+            totalCount: response.data.count,
+          });
+          setThumbnails(response?.data.thumbnails || []);
         }
       } catch (error) {
         console.error("Error fetching thumbnails:", error);
@@ -35,49 +54,104 @@ const ExtractPage = () => {
     };
 
     fetchThumbnails();
-  }, []);
+  }, [id, page]);
 
-  const togglePage = (index: number) => {
-    setSelectedPages((prev) =>
-      prev.includes(index) ? prev.filter((p) => p !== index) : [...prev, index],
-    );
+  async function generateNewPDF() {
+    setGenerateNewPdf(true);
+    try {
+      if (!id) {
+        console.log("pdf id missing");
+        return;
+      }
+
+      const selectedIndex = selectedPages.map((d) => d.page);
+
+      const promise = generateExtractPdf(id, selectedIndex);
+      toast.promise(promise, {
+        loading: "Generating PDF...",
+        success: "PDF generated successfully",
+        error: "Failed to generate PDF",
+      });
+
+      const res = await promise;
+      downloadFile(res.data, "Generated_Document.pdf", "application/pdf");
+      console.log("Generating PDF with pages:", selectedPages);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setGenerateNewPdf(false);
+    }
+  }
+
+  const togglePage = (data: IThumbnail) => {
+    setSelectedPages((prev) => {
+      const exists = prev.find((p) => p.page === data.page);
+
+      if (exists) {
+        return prev.filter((p) => p.page !== data.page);
+      }
+
+      return [...prev, data];
+    });
   };
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <Spinner />
-      </div>
+      <>
+        <Header fileName={"loading"} totalCount={0} />
+        <div className="flex h-full overflow-hidden p-2 md:px-10">
+          <ThumbnailSkeleton limit={limit} />
+          <SidebarSkeleton />
+        </div>
+      </>
     );
   }
 
   return (
-    <div className="flex h-screen">
-      {/* LEFT SIDE - THUMBNAILS */}
-      <div className="flex-1 overflow-auto p-6">
-        <div className="grid grid-cols-4 gap-6">
-          {thumbnails.length > 0 ? (
-            thumbnails.map((thumbnail, index) => (
-              <ThumbnailCard
-                key={index}
-                pageNumber={thumbnail.page}
-                thumbnail={thumbnail.thumbnail}
-                selected={selectedPages.includes(thumbnail.page)}
-                // onClick={() => togglePage(index)}
-              />
-            ))
-          ) : (
-            <p>No thumbnails found.</p>
-          )}
+    <>
+      <Header
+        fileName={pdfDetails.fileName}
+        totalCount={pdfDetails.totalCount}
+      />
+      <div className="flex h-full overflow-hidden p-2">
+        <div className="flex-1 overflow-auto lg:px-28 ">
+          <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-6">
+            {" "}
+            {thumbnails.length > 0 ? (
+              thumbnails.map((thumbnail, index) => (
+                <ThumbnailCard
+                  key={index}
+                  pageNumber={thumbnail.page}
+                  thumbnail={thumbnail.thumbnail}
+                  selected={selectedPages.some(
+                    (p) => p.page === thumbnail.page,
+                  )}
+                  onSelect={() => togglePage(thumbnail)}
+                />
+              ))
+            ) : (
+              <p>No thumbnails found.</p>
+            )}
+          </div>
         </div>
+
+        <SelectedPagesSidebar
+          selectedPages={selectedPages}
+          onRemove={(page: number) =>
+            setSelectedPages((prev) => prev.filter((p) => p.page !== page))
+          }
+          generateNewPDF={generateNewPDF}
+          isgenerateNewPdf={isgenerateNewPdf}
+        />
       </div>
 
-      {/* RIGHT SIDE - SELECTED PAGES */}
-      <SelectedPagesSidebar
-        selectedPages={thumbnails}
-        onRemove={(id: number) => console.log(id)}
+      <Pagination
+        currentPage={page}
+        limit={limit}
+        totalCount={pdfDetails.totalCount}
+        onPageChange={(newPage) => setPage(newPage)}
       />
-    </div>
+    </>
   );
 };
 
