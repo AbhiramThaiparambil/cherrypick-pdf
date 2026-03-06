eadadd64554992d022c9d3476af09cf6c02bbd0b
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-03-06 22:12:05 +0530

    feat(server): implement generate new PDF use case with proper interface and application layer

---
+++ server/src/application/use-case/generateNewPdf/GeneratePdfUseCase.ts
@@ -0,0 +1,39 @@
+import { inject, injectable } from "tsyringe";
+import {
+  GeneratePdfRequestDTO,
+  GeneratePdfResponseDTO,
+} from "../../dtos/usecase/GeneratePdf.dto";
+import { IGeneratePdfUseCase } from "./IGeneratePdfUseCase";
+import { REPOSITORY_TOKEN, SERVICE_TOKEN } from "../../../constant/tocken";
+import { IPdfRepository } from "../../../domain/repositories/IPdfRepository";
+import { IPdfExtractService } from "../../../services/PdfExtract/IPdfExtract.service";
+
+@injectable()
+export class GeneratePdfUseCase implements IGeneratePdfUseCase {
+  constructor(
+    @inject(SERVICE_TOKEN.PDF_EXTRACT_SERVICE)
+    private pdfExtractService: IPdfExtractService,
+    @inject(REPOSITORY_TOKEN.PDF_REPOSITORY)
+    private pdfRepository: IPdfRepository,
+  ) {}
+
+  async execute(
+    request: GeneratePdfRequestDTO,
+  ): Promise<GeneratePdfResponseDTO> {
+    const { pdfId, pages } = request;
+    const pdfData = await this.pdfRepository.getPdfById(pdfId);
+
+    if (!pdfData) {
+      throw new Error("PDF not found");
+    }
+
+    const filePath = pdfData.originalPdfPath.path;
+
+    const buffer = await this.pdfExtractService.extractPages(filePath, pages);
+
+    return {
+      fileName: `${pdfData.originalPdfPath.fileName}_cherry_pick.pdf`,
+      fileBuffer: buffer,
+    };
+  }
+}
