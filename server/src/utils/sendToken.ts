db74c35a0d01feadfb5dd5240000e944b6a867ab
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-03-08 01:03:33 +0530

    feat(server): add helper functions for sending access and refresh tokens

---
+++ server/src/utils/sendToken.ts
@@ -0,0 +1,17 @@
+import { Response } from "express";
+import { HTTP_STATUS } from "../constant/httpStatus";
+
+export const sendRefreshToken = (res: Response, refreshToken: string) => {
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+};
+
+export const sendAccessToken = (res: Response, accessToken: string) => {
+  return res.status(HTTP_STATUS.OK).json({
+    accessToken,
+  });
+};
