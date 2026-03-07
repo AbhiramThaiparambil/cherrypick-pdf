e837009012bc9ff2b657f92537be6a4ed817db3f
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-03-07 22:57:26 +0530

    (server)implement user repository

---
+++ server/src/infrastructure/repositories/User.repository.ts
@@ -0,0 +1,46 @@
+import { injectable } from "tsyringe";
+import { IUserRepository } from "../../domain/repositories/IUserRepository";
+import { IUser } from "../../domain/entities/IUser";
+import { UserModel } from "../database/user.model";
+import bcrypt from "bcrypt";
+@injectable()
+export class UserRepository implements IUserRepository {
+  async create(data: Partial<IUser>): Promise<IUser> {
+    const user = await UserModel.create({
+      email: data.email,
+      password: data.password,
+      createdAt: new Date(),
+    });
+
+    return {
+      _id: user._id.toString(),
+      email: user.email,
+      password: user.password,
+      createdAt: user.createdAt,
+    };
+  }
+
+  async findByEmail(email: string): Promise<IUser | null> {
+    const user = await UserModel.findOne({ email }).lean().exec();
+
+    if (!user) return null;
+
+    return {
+      _id: user._id.toString(),
+      email: user.email,
+      password: user.password,
+      createdAt: user.createdAt,
+    };
+  }
+
+  comparePassword(
+    userInputPassword: string,
+    storedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(userInputPassword, storedPassword);
+  }
+
+  hashPassword(password: string, saltRounds: number = 10): Promise<string> {
+    return bcrypt.hash(password, saltRounds);
+  }
+}
