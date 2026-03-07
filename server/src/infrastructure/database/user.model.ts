e65b8096b8ff3518b99ea821f5a014718174d8d3
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-03-07 22:56:33 +0530

    feat(server): add user schema

---
+++ server/src/infrastructure/database/user.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+export const UserModel = model<IUser>("User", userSchema);
