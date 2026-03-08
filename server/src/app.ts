66bc226ba0e99fc631023dbffc15f196ef05c03b
AbhiramThaiparambil <abhiramthaipparambil@gmail.com>
2026-03-08 20:56:09 +0530

    (server)fix backend .env config issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,12 +5,16 @@ import pdfRoute from "./presentation/routes/pdf.routes";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 
 import cors from "cors";
 import authRouter from "./presentation/routes/auth.routes";
 import { errorHandler } from "./presentation/middlewares/errorHandler.middleware";
 app.use(morgan("dev"));
+console.log("FRONTEND_URL:"+process.env.FRONTEND_URL)
 app.use(
   cors({
     origin: process.env.FRONTEND_URL ,
